6fd64c5f9b9aba443e3918f1065de74acccdfd3c
Zachary Griffee <zacharygriffee@gmail.com>
2024-01-09 10:16:32 -0500

    Structuring, Browser compat, Minification - Ensure browser compatibility.   - Tests that run in node now can be run in browser with     browser-tests.html in root of pkg   - Tests in html file demonstrate hyperdrive usage - Bundle of each section and a bundle of the project as a whole - Exports of each section for tree-shake for both browser and default

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export * from "./lib/find/index.js";
-export * from "./lib/query/index.js";
-export * from "./lib/resolve/index.js"
-export * from "./lib/deploy/index.js";
-export * from "./lib/adapt/index.js";
+export * as Find from "./lib/find/index.js";
+export * as Query from "./lib/query/index.js";
+export * as Resolve from "./lib/resolve/index.js"
+export * as Deploy from "./lib/deploy/index.js";
+export * as Adapt from "./lib/adapt/index.js";
 export {default as path} from "tiny-paths";
 
