08f75dfa237ef372c677db17bc70f3215f3726df
Zachary Griffee <zacharygriffee@gmail.com>
2024-01-14 11:44:34 -0500

    wasm was corrupted somehow

--- tests/all.js
+++ tests/all.js
@@ -1,11 +1,6 @@
-// if (typeof process !== "undefined" && process?.versions?.node) {
-//     const {default: fetch} = await import("node-fetch".toString());
-//     globalThis.fetch = fetch;
-// }
-
 await import("./find.js");
 await import("./query.js");
-if (!globalThis.window) await import("./deploy.js");
+await import("./deploy.js");
 await import("./resolve.js");
 await import("./adapt.js");
 await import("./transport.js");
